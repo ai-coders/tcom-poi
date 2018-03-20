5d5bf832299e111f6e79bba00ce9998a5f30dcf2
stonethink <stonethink@163.com>
2018-03-20 18:48:51 +0800

    Init project of Spring boot

---
+++ src/main/java/net/aicoder/tcompoi/TcomPoiApplication.java
@@ -0,0 +1,12 @@
+package net.aicoder.tcompoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcomPoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcomPoiApplication.class, args);
+	}
+}
